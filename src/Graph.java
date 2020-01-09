789bd8f38263abdd42dcdd2a096fbbda17a2e998
Rafael Piacsek de Queiroz Ferreira <rafaelpiacsek@Rafaels-MacBook-Pro.local>
2020-01-09 12:22:14 -0500

    Initial commit

---
+++ src/Graph.java
@@ -0,0 +1,19 @@
+
+public class Graph<DataType> {
+
+    /* Fields */
+
+
+    /* Methods */
+
+    // addNode
+    // removeNode
+    // addEdge
+    // removeEdge
+    // getEdges
+    // getNodes
+
+
+    /* Classes */
+
+}
